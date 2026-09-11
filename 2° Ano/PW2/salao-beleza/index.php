e8c974d7ef54488fe4b611cd38aa9c14e4e8f356
Douglas Soares <douglas201003@gmail.com>
2026-09-11 13:40:13 -0300

    Remove CSS comment in index.php
    Delete an inline HTML comment that noted the stylesheet was fully custom

--- 2° Ano/PW2/salao-beleza/index.php
+++ 2° Ano/PW2/salao-beleza/index.php
@@ -34,7 +34,6 @@ $total = count($agendamentos);
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,500;0,600;1,500&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <!-- CSS 100% próprio (sem Bootstrap) -->
     <link href="css/estilo.css" rel="stylesheet">
 </head>
 <body>
